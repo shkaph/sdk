5417d7d88e39fa6c44d881287ec501bfcd2f8d03
Olexiy Shkarupa <ext-olexiy.shkarupa@here.com>
2019-05-26 17:26:05 +0300

    Server (python) sends playfield as JSON Client (html) recieves JSON and changes state of grid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# sudo apt install python3-pip
+# pip3 install websocke
+
 import sys
 import asyncio
 import websockets
@@ -8,39 +11,63 @@ import json
 
 
 class Cell:
-	options	 = [ i for i in range(1, 10) ]
+	options = [ i for i in range(1, 10) ]
 	value = -1
+
+	def serialize(self):
+		res = '{'
+		res += ' "options" : ' + json.dumps(self.options) + ','
+		res += ' "value": ' + str( self.value )
+		res += ' }'
+		return res
 
 class Playfield:
 	cells = []
 
 	def __init__(self):
 		for i in range(0, 9):
-			cells.insert([])
+			row = []
 			for j in range(0, 9):
-				cells[i].insert(Cell())
+				row.append( Cell() )
+			self.cells.append( row )
 
-	'''
-	def __init__(self):
-		:
-			self.digits.append(i + 20)'''
+	def serialize(self):
+		res = '{ "cells" : ['
+		i = 0
+		for row in self.cells:
+			res += ' ['
+			j = 0
+			for cell in row:
+				res += cell.serialize()
+				j += 1
+				if j < 9:
+					res += ', '
+			i += 1
+			if i < 9:
+				res += '],'
+			else:
+				res += ']'
+		res += ' ] }'
+		return res
 
-stencil = Stencil()
-print(stencil.digits)
-demo = json.dumps(stencil.digits)
+'''
+stencil = Cell()
+print(stencil.options)
+demo = json.dumps(stencil.options)
 print(demo)
 
-print(20 in stencil.digits)
-print(24 in stencil.digits)
+print(7 in stencil.options)
+print(24 in stencil.options)
 
 k = [i for i in range(1, 6)]
 print(k)
 
+
 # some JSON:
 x =  '{ "name":"John", "age":30, "city":"New York", "IDs" : [2, 7, 456]}'
-
 # parse x:
 y = json.loads(x)
+
 
 # the result is a Python dictionary:
 #print(y["age"])
@@ -48,8 +75,11 @@ y = json.loads(x)
 print(y)
 z = json.dumps(y)
 print(z)
+'''
 
-
+playfield = Playfield()
+y = playfield.serialize()  # fake serialization - do not se it!
+#print(y)
 
 
 
@@ -57,17 +87,14 @@ async def hello(websocket, path):
     name = await websocket.recv()
     print(f"< {name}")
 
-    greeting = f"Hello {name}!"
-
-    #await websocket.send(greeting)
-    await websocket.send(z)
+    await websocket.send(y)
     #print(f"> {greeting}")
-    print(f"> {z}")
+    #print(f"> {y}")
 
 start_server = websockets.serve(hello, 'localhost', 8765)
 
-#asyncio.get_event_loop().run_until_complete(start_server)
-#asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
 
 
 
